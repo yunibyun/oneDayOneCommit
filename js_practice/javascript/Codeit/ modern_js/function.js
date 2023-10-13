5feae0052d455a7fdf45efb5a5d5650e3f4f33a2
yunibyun <yunabyun@icloud.com>
2023-10-13 20:37:21 +0200

    aaa

---
+++ js_practice/javascript/Codeit/ modern_js/function.js
@@ -0,0 +1,12 @@
+
+// function declaration
+function nameFunc (Param) {
+  return something;
+}
+
+// 함수 선언을 값처럼 활용하는 방식
+// function expression
+const printSomething () = function () {
+  console.log('Print Something!');
+}
+
